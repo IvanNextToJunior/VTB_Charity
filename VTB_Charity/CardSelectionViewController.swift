c153f24c9207a61e26e5c23aa6cd5c04f7c46af1
IvanNextToJunior <Vanwan@bk.ru>
2021-11-26 14:05:26 +0300

    add CardSelectionViewController

---
+++ VTB_Charity/CardSelectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CardSelectionViewController.swift
+//  VTB_Charity
+//
+//  Created by Ivan on 26.11.2021.
+//
+
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}
