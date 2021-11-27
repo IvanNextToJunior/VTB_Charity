def76592d7f6df1cc8e55b9812caa825e0fa699d
IvanNextToJunior <Vanwan@bk.ru>
2021-11-27 21:12:16 +0300

    register CardCollectionViewCell

--- VTB_Charity/CardSelectionViewController.swift
+++ VTB_Charity/CardSelectionViewController.swift
@@ -9,17 +9,19 @@ import UIKit
 
 class CardSelectionViewController: UIViewController {
 
+    @IBOutlet weak private var selectCardCollectionView: UICollectionView!
     
-    @IBOutlet weak var selectCardCollectionView: UICollectionView!
-    
-    
+    private let identifier = "cardCollectionViewReuseIdentifier"
+    private let nib = UINib(nibName: "CardCollectionViewCell", bundle: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    private func setup() {
+        selectCardCollectionView.register(<#T##nib: UINib?##UINib?#>, forCellWithReuseIdentifier: <#T##String#>)
+    }
   
 
 }
