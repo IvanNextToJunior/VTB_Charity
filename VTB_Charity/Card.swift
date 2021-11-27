0cbd2fb7b322ccb2f067a952ccb586ad71ec32da
IvanNextToJunior <Vanwan@bk.ru>
2021-11-27 22:06:22 +0300

    connect menu outlets; remove Card file

--- VTB_Charity/Card.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Card.swift
-//  VTB_Charity
-//
-//  Created by Ivan on 27.11.2021.
-//
-
-import Foundation
--- VTB_Charity/MenuViewController.swift
+++ VTB_Charity/MenuViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class MenuViewController: UIViewController {
+    @IBOutlet weak var cardButton: UIButton!
+    
+    @IBOutlet weak var moneyInputButton: UIButton!
+    
+    @IBOutlet weak var moneyTransferButton: UIButton!
+    
+    @IBOutlet weak var pointsTrackerButton: UIButton!
+    
+    @IBOutlet weak var charityFondButton: UIButton!
+    
+    @IBOutlet weak var personalAccountButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
