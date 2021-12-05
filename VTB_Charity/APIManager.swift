60a802fd0c44b0e64cac2797e315f0b80dcc03b2
IvanNextToJunior <Vanwan@bk.ru>
2021-12-05 22:46:27 +0300

    Update APIManager.swift

---
+++ VTB_Charity/APIManager.swift
@@ -0,0 +1,16 @@
+//
+//  APIManager.swift
+//  VTB_Charity
+//
+//  Created by Ivan on 04.12.2021.
+//
+
+import Foundation
+
+struct APIManager {
+    
+    //MARK: API References
+    var links = [
+        "https://epa.api.vtb.ru/openapi/rb/dks/cardemission/hackathon/v1/prepaid"
+    ]
+}
