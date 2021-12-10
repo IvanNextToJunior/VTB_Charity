db2ac7f95bcaaa28e805b9dccad6d95f957cd52f
IvanNextToJunior <Vanwan@bk.ru>
2021-12-10 14:30:04 +0300

    Update APIManager.swift

--- VTB_Charity/APIManager.swift
+++ VTB_Charity/APIManager.swift
@@ -10,24 +10,25 @@ import Foundation
 struct APIManager {
     
     //MARK: API References
-    var links = [
-        "https://hackaton.bankingapi.ru/api/rb/dks/cardemission/hackathon/v1/prepaid?X-MDM-Id=id&Authorization=Bearer{token}&x-client-channel=team27@app.hackaton.bankingapi.ru&X-PARTNER-ID=15522222&body{partnerId=15522222}",
-        "https://hackaton.bankingapi.ru/api/rb/dks/cardops/hackathon/v1/pin/{publicId}?X-MDM-Id=id&Authorization=Bearer{token}&x-client-channel=team27@app.hackaton.bankingapi.ru&X-PARTNER-ID=15522222&publicId=1111111111111&body{pin:hssdsdssss,publicKeyId=1111111111111}"
-   ]
+    
+//        "https://hackaton.bankingapi.ru/api/rb/dks/cardemission/hackathon/v1/prepaid?X-MDM-Id=id&Authorization=Bearer{token}&x-client-channel=team27@app.hackaton.bankingapi.ru&X-PARTNER-ID=15522222&body{partnerId=15522222}",
+//        "https://hackaton.bankingapi.ru/api/rb/dks/cardops/hackathon/v1/pin/{publicId}?X-MDM-Id=id&Authorization=Bearer{token}&x-client-channel=team27@app.hackaton.bankingapi.ru&X-PARTNER-ID=15522222&publicId=1111111111111&body{pin:hssdsdssss,publicKeyId=1111111111111}"
+   
 
-    func getRequest(for urlAtIndex: Int, with method: String) -> URLRequest {
-       
-        let urlString = links[urlAtIndex]
+    private func makeRequest(for urlString: String, with method: String, bodyParams: String, headers: [String:String]) -> URLRequest {
+    
+        
         guard let url = URL(string: urlString) else { fatalError()}
         var request = URLRequest(url: url)
         request.httpMethod = method
-         
+        request.httpBody = Data(base64Encoded: bodyParams)
+        request.allHTTPHeaderFields = headers
         return request
     }
 
-    func parseResponse(for urlAtIndex: Int, with method: String) {
-        let request = getRequest(for: urlAtIndex, with: method)
+  //  func upload(result: ) ->   {
         
-    }
+  //  }
+  
 
 }
